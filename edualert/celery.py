adf20a199eb1eaf85cb854d73610a92c8c8b12c1
madalinagal <madalina.gal@rodeapps.com>
2021-01-04 09:58:33 +0200

    Commented weekly alerts for grades and absences.

--- edualert/celery.py
+++ edualert/celery.py
@@ -34,10 +34,10 @@ app.conf.beat_schedule = {
         'task': 'edualert.catalogs.tasks.send_alerts_for_risks_task',
         'schedule': crontab(day_of_week=1, hour=6, minute=30)
     },
-    'send_alerts_for_school_situation_task': {
-        'task': 'edualert.catalogs.tasks.send_alerts_for_school_situation_task',
-        'schedule': crontab(day_of_week=1, hour=10, minute=0)
-    },
+    # 'send_alerts_for_school_situation_task': {
+    #     'task': 'edualert.catalogs.tasks.send_alerts_for_school_situation_task',
+    #     'schedule': crontab(day_of_week=1, hour=10, minute=0)
+    # },
     'calculate_students_placements_task': {
         'task': 'edualert.catalogs.tasks.calculate_students_placements_task',
         'schedule': crontab(hour=23, minute=59),
